4238cceafa6757b002bc6a973af55ca42ec74835
Chad Durkin <DurkinDevelopment@gmail.com>
2022-03-29 09:13:27 -0500

    feat: Added the circular linked list class and test file

---
+++ python/linked_list/circular_linked_list/circular_linked_list.py
@@ -0,0 +1,21 @@
+class Node():
+    def __init__ (self, data = None):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList():
+    def __init__ (self):
+        self.head = None
+        self.count = 0
+    
+    def insert(self, data):
+
+    def remove_by_index(self, index):
+
+    def remove_by_node(self, node):
+
+    def retrieve_by_index(self, index):
+
+    def print_from_head(self):
+
+    def print_from_tail(self):
